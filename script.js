da39a0a6c49c2808ced139080af5b780085b68b1
sander <sander.sohngen@usp.br>
2021-12-27 14:49:12 -0300

    finished random mode

--- script.js
+++ script.js
@@ -1,17 +1,112 @@
+const playerFactory = (() => {
+	const Player = (icon, winMessage, turnMessage, marker) => {
+		const getIcon = () => icon;
+		const getWinnerMessage = () => winMessage;
+		const getTurnMessage = () => turnMessage;
+		const getMarker = () => marker;
+		const isComputer = () => false;
+		return {
+			getIcon,
+			getWinnerMessage,
+			getTurnMessage,
+			getMarker,
+			isComputer,
+		};
+	};
+
+	const pizzaPlayer = () => {
+		const pizza = "img/pizza.svg";
+		const winMessage = "Pizza Player won";
+		const turnMessage = "Burger Player turn";
+		const marker = 1;
+		const prototype = Player(pizza, winMessage, turnMessage, marker);
+		return prototype;
+	};
+
+	const burgerPlayer = () => {
+		const burger = "img/burger.svg";
+		const winMessage = "Burger Player won";
+		const turnMessage = "Pizza Player turn";
+		const marker = -1;
+		const prototype = Player(burger, winMessage, turnMessage, marker);
+		return prototype;
+	};
+
+	const compBurguerPlayer = () => {
+		const prototype = burgerPlayer();
+		const isComputer = () => true;
+		const playRound = (gameboard) => {
+			const openSlotsCounter = countOpenSlots(gameboard);
+			const slotPlayed = findSlotToPlay(gameboard, openSlotsCounter);
+			return slotPlayed;
+		};
+		function countOpenSlots(gameboard) {
+			let openSlotsCounter = 0;
+			for (let i = 0; i < gameboard.length; i++)
+				if (gameboard[i] === 0) openSlotsCounter++;
+			return openSlotsCounter;
+		}
+		function findSlotToPlay(gameboard, openSlotsCounter) {
+			const index = Math.floor(Math.random() * openSlotsCounter);
+			let openSlots = [];
+			for (let i = 0; i < gameboard.length; i++)
+				if (gameboard[i] === 0) openSlots.push(i);
+			return openSlots[index];
+		}
+		return Object.assign({}, prototype, { playRound, isComputer });
+	};
+
+	return {
+		createPizzaPlayer: pizzaPlayer,
+		createBurgerPlayer: burgerPlayer,
+		createBurgerCompPlayer: compBurguerPlayer,
+	};
+})();
+
 const game = (() => {
 	const gameboard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-	const firstMessage = "Pizza Player turn";
-	const secondMessage = "Burger Player turn";
-	const firstWon = "Pizza Player won";
-	const secondWon = "Burger Player won";
+	const waitingMessage = "Select a vs mode";
 	const drawMessage = "It's a draw";
 	const message = document.querySelector("#message");
 	const squares = document.querySelectorAll(".square");
-	const pizza = "img/pizza.svg";
-	const burger = "img/burger.svg";
 
-	const restart = document.querySelector("#restart");
-	restart.addEventListener("click", resetBoard);
+	const pizzaPlayer = playerFactory.createPizzaPlayer();
+	let burgerPlayer = playerFactory.createBurgerPlayer();
+
+	const options = {
+		humanPlayer: changeToHumanPlayer,
+		restart: resetBoard,
+		computerPlayer: changeToComputerPlayer,
+	};
+
+	const buttons = document.querySelector("#options");
+	buttons.addEventListener("click", (event) => {
+		const id = event.target.id;
+		if (options[id]) options[id]();
+	});
+
+	function changeToHumanPlayer() {
+		const selectedsID = "humanPlayer";
+		const deselectedsID = "computerPlayer";
+		highlightVsMode(selectedsID, deselectedsID);
+		burgerPlayer = playerFactory.createBurgerPlayer();
+		resetBoard();
+	}
+
+	function changeToComputerPlayer() {
+		const selectedsID = "computerPlayer";
+		const deselectedsID = "humanPlayer";
+		highlightVsMode(selectedsID, deselectedsID);
+		burgerPlayer = playerFactory.createBurgerCompPlayer();
+		resetBoard();
+	}
+
+	function highlightVsMode(selectedsID, deselectedsID) {
+		const selected = document.querySelector(`#${selectedsID}`);
+		const deselected = document.querySelector(`#${deselectedsID}`);
+		selected.classList.add("selected");
+		deselected.classList.remove("selected");
+	}
 
 	function resetBoard() {
 		clearGameboard(gameboard);
@@ -38,7 +133,7 @@ const game = (() => {
 	}
 
 	function resetMessage() {
-		message.textContent = firstMessage;
+		message.textContent = waitingMessage;
 	}
 
 	function addListeners(squares) {
@@ -50,6 +145,16 @@ const game = (() => {
 	function playRound(event) {
 		const id = event.target.id;
 		const playerTurn = identifyTurn(gameboard);
+		console.table(playerTurn);
+		placePlay(id, playerTurn, gameboard);
+		changeDisplayMessage(playerTurn);
+		const gameEnded = checkIfGameEnded(gameboard, playerTurn);
+		if (!gameEnded && burgerPlayer.isComputer()) playComputerRound(gameboard);
+	}
+
+	function playComputerRound(gameboard) {
+		const id = "_" + burgerPlayer.playRound(gameboard);
+		const playerTurn = burgerPlayer;
 		placePlay(id, playerTurn, gameboard);
 		changeDisplayMessage(playerTurn);
 		checkIfGameEnded(gameboard, playerTurn);
@@ -57,12 +162,12 @@ const game = (() => {
 
 	function identifyTurn(gameboard) {
 		const playerTurn = gameboard.reduce((sum, current) => sum + current);
-		return playerTurn === 0 ? 1 : -1;
+		return playerTurn === 0 ? pizzaPlayer : burgerPlayer;
 	}
 
 	function placePlay(id, playerTurn, gameboard) {
 		const idNumber = id[1]; // because HTML id can't be number, they are formated as _number
-		gameboard[idNumber] = playerTurn;
+		gameboard[idNumber] = playerTurn.getMarker();
 		const square = document.querySelector(`#${id}`);
 		adjustDivPlayed(square, playerTurn);
 	}
@@ -80,12 +185,12 @@ const game = (() => {
 
 	function appendImageToSquare(square, playerTurn) {
 		const img = document.createElement("img");
-		img.src = playerTurn === 1 ? pizza : burger;
+		img.src = playerTurn.getIcon();
 		square.appendChild(img);
 	}
 
 	function changeDisplayMessage(playerTurn) {
-		message.textContent = playerTurn === 1 ? secondMessage : firstMessage;
+		message.textContent = playerTurn.getTurnMessage();
 	}
 
 	function checkIfGameEnded(gameboard, playerTurn) {
@@ -96,6 +201,7 @@ const game = (() => {
 		const reachedDraw = checkDraw(gameboard);
 		const gameEnded = aPlayerWon || reachedDraw;
 		if (gameEnded) endGame(playerTurn, aPlayerWon);
+		return gameEnded;
 	}
 
 	function checkRows(gameboard) {
@@ -145,9 +251,7 @@ const game = (() => {
 
 	function displayWinnerMessage(playerTurn, aPlayerWon) {
 		message.textContent = aPlayerWon
-			? playerTurn === 1
-				? firstWon
-				: secondWon
+			? playerTurn.getWinnerMessage()
 			: drawMessage;
 	}
 
@@ -157,64 +261,3 @@ const game = (() => {
 })();
 
 game.resetBoard();
-
-const playerFactory = (() => {
-	const Player = (icon, winMessage, turnMessage, marker) => {
-		const getIcon = () => icon;
-		const displayWinnerMessage = () => winMessage;
-		const displayTurnMessage = () => turnMessage;
-		const getMarker = () => marker;
-		return { getIcon, displayWinnerMessage, displayTurnMessage, getMarker};
-	};
-
-	const pizzaPlayer = () => {
-		const pizza = "img/pizza.svg";
-		const winMessage = "Pizza Player won";
-		const turnMessage = "Pizza Player turn";
-		const marker = 1;
-		const prototype = Player(pizza, winMessage, turnMessage, marker);
-		return prototype;
-	};
-
-	const burgerPlayer = () => {
-		const burger = "img/burger.svg";
-		const winMessage = "Burger Player won";
-		const turnMessage = "Burger Player turn";
-		const marker = -1;
-		const prototype = Player(burger, winMessage, turnMessage, marker);
-		return prototype;
-	};
-
-	const compBurguerPlayer = () => {
-		const prototype = burgerPlayer();
-		const playRound = (gameboard) => {
-			const openSlots = countOpenSlots(gameboard);
-			const slotPlayed = findSlotToPlay(gameboard, openSlots);
-			return slotPlayed;
-		};
-		function countOpenSlots(gameboard) {
-			const openSlots = 0;
-			for (let i = 0; i < gameboard.length; i++)
-				if (gameboard[i] === 0) openSlots++;
-			return openSlots;
-		}
-		function findSlotToPlay(gameboard, openSlots) {
-			const index = Math.floor(Math.random() * openSlots);
-			let openSlots = [];
-			for (let i = 0; i < gameboard.length; i++)
-				if(gameboard[i] === 0) openSlots.push(i);
-			return openSlots[index];
-		}
-		return Object.assign({}, prototype, {playRound});
-	};
-
-	createPizzaPlayer = () => pizzaPlayer;
-	createBurgerPlayer = () => burgerPlayer;
-	createBurgerCompPlayer = () => compBurguerPlayer;
-
-	return {
-		createPizzaPlayer,
-		createBurgerPlayer,
-		createBurgerCompPlayer,
-	}
-})();
